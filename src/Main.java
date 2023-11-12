d5f25f4b3eda68621626eb3d54a19d13465f140a
Anton Sergeev <sergant3082605@mail.com>
2023-11-12 23:35:08 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 300;
+        int cash = 150;
+        int bonus = 0;
+        int finalBalance = initialBalance + cash;
+        if (cash >= 1000) {
+            bonus = cash / 100;
+            finalBalance += bonus;
+        }
+        System.out.println("Итоговый баланс: " + finalBalance + " рублей\nНачислено бонусов: " + bonus);
+    }
+}
