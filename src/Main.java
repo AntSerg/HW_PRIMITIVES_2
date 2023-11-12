9378429aabaf31ae607e6a9d1954672ab52dba97
Anton Sergeev <sergant3082605@mail.com>
2023-11-12 23:44:18 +0400

    Change class Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 300;
-        int cash = 150;
+        int cash = 1250;
         int bonus = 0;
         int finalBalance = initialBalance + cash;
-        if (cash >= 1000) {
+        if (cash > 1000) {
             bonus = cash / 100;
             finalBalance += bonus;
         }
